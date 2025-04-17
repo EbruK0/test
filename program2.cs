ab0799cb7f59f0bf4f885a67ceb69b9c70f5dc55
EbruK0 <ebruhuseinova5@gmail.com>
2025-04-17 11:15:43 +0300

    Add files via upload

---
+++ program2.cs
@@ -0,0 +1,22 @@
+﻿namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string input = Console.ReadLine();
+            string[] part = input.Split(' ');
+            int[] numbers = new int[part.Length];
+                int sum = 0;
+            
+            for (int i = 0; i < part.Length; i++)
+            {
+                numbers[i] = int.Parse(part[i]);
+                sum += numbers[i];
+            }
+            Console.WriteLine($"Сумата на елементите: {part}");
+            Console.WriteLine($"Най-голям елемент: {part}");
+            Console.WriteLine($"Числото 9 се среща {part} пъти");    
+        }
+    }
+}
